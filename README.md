136f32b1890c7cbe5c64ed87951e812672d95fc4
Bruno <stamby@github.com>
2021-10-26 18:44:31 -0300

    First upload

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cm-to-ft-in
 A script that converts CM to feet and inches.
+
+```
+Usage: cm-to-ft-in CM
+```
---
+++ cm-to-ft-in.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+### cm-to-ft-in: Converts CM to feet and inches.
+
+[ $# -ne 1 ] && {
+    cat << EOF
+Usage: $0 CM
+EOF
+    exit 1
+}
+
+format_() {
+    printf "%s'%1.1f\"\n" $@
+}
+
+format_ $(
+bc << EOF
+a = $1
+a / 30.48
+b = a % 30.48
+scale = 1
+b / 2.54
+EOF
+)
+
